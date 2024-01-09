979bf4115252222e3d917edf3a720a8b1dc45a9f
georg <georg@GEORGETH>
2024-01-10 00:02:07 +0200

    ααα

--- UnitTestHRLib/UnitTestPersonnel.cs
+++ UnitTestHRLib/UnitTestPersonnel.cs
@@ -76,10 +76,10 @@ namespace HRLibUnitTest
         [TestMethod]
         public void TestMethodValidPassword() /* THEO */
         {
-            
-            
-                // Δημιουργία ενός αντικειμένου της κλάσης Personnel του HRLib.dll που θέλουμε να τεστάρουμε
-                HRLib.Personnel per = new HRLib.Personnel();
+
+
+            // Δημιουργία ενός αντικειμένου της κλάσης Personnel του HRLib.dll που θέλουμε να τεστάρουμε
+            HRLib.Personnel per = new HRLib.Personnel();
             /*  Γράψ'τε σε μία κενή γραμμή per. (π.χ. την 18) και θα σας εμφανίσει τις μεθόδους...
              *  Έχει και άλλες 4 που είναι μέθοδοι της κλάσης object που κληρονομούν ΌΛΕΣ οι κλάσεις που φτιάχνουμε
              */
@@ -93,98 +93,29 @@ namespace HRLibUnitTest
              *    μήνυμα που αιτιολογεί τις πληροφορίες που περιμένω
              *  }
              */
-         /*[1] Τουλάχιστον 12 χαρακτήρες
-         * [2] Συνδυασμός χαρακτηριών
-         * [3] Τα γράμματα να είναι λατινικοί χαρακτήρες
-         * [4] Να ξεκινάει από κεφαλαίο γράμμα και να τελειώνει με αριθμό
+         /*προδιαγραφες κωδικου(password)
+         * [1] Τουλάχιστον 12 χαρακτήρες
+         * [2] Tο πολυ 24 χαρακτηρες
+         * [3] Συνδυασμός χαρακτηριών
+         * [3.1] τουλαχιστον 1 κεφαλαιο 
+         * [3.2] τουλαχιστον ενα πεζο
+         * [3.3] τουλαχισοτν 1 νουμερο
+         * [3.4] τουλαχιαοτν 1 ειδικο συμβολο
+         * [4] χαρακτηρες διαφυγης 
+         * [5] Τα γράμματα να είναι λατινικοί χαρακτήρες
+         * [6] Να ξεκινάει από κεφαλαίο γράμμα και να τελειώνει με αριθμό
          */
-                { 1, "Ako2", false, "περιεχει λιγοτερα απο 12 χαρακτηρες" },
-                { 2, "Akkakanhfhfhrbfhbyrbtybytbh4#$fhbrvhvb1",false,"περιεχει παραπανω 24 χαρακτηρες" },
-                { 3, "Vasilisathan8#6" ,true,"περιεχει πανω απο 12 χαρακτηρεσ ξεκινα απο κεφαλαιο ,εχει συμβολα  και τελειωνει σε αριθμο"},
-                { 4, "γιωργοςπαδα2024",false,"δεν περιεχει λατινικους χαρακτηρες"},
-                { 5, "omaraAlhaz2001",false,"ξεκινα με μικρο γραμμα"},
-                { 6, "Koaos^klpoplkjg",false,"δεν τελειωνει σε αριθμο" },
-                { 7, "Georgetheo@#$9",true,"περιεχει πανω απο 12 χαρακτηρεσ ξεκινα απο κεφαλαιο ,εχει συμβολα  και τελειωνει σε αριθμο" },
+                { 1, "Ako2", false, "    [1] Τουλάχιστον 12 χαρακτήρες" },
+                { 2, "Akkakanhfhfhrbfhbyrbtybytbh4#$fhbrvhvb1",false,"[2] Tο πολυ 24 χαρακτηρες" },
+                { 3, "Vasilisathanο#6" ,true," εγκυρος κωδκος "},
+                { 4, "γιωργοςπαδα2024",false,"[5] Τα γράμματα να είναι λατινικοί χαρακτήρες"},
+                { 5, "omaraAlhaz2001",false," [6]  Να ξεκινάει από κεφαλαίο γράμμα και να τελειώνει με αριθμό"},
+                { 6, "Koaos^klpoplkjg",false,"[6] Να ξεκινάει από κεφαλαίο γράμμα και να τελειώνει με αριθμό" },
+                { 7, "Georgetheo@#$9",true,"εγκυρος κωδικος " },
+                { 8,"!@#$%^&**&^%$#@!",false,"[3] Συνδυασμός χαρακτηριών" },
+
             };
-            
 
-                // Αρχικοποίηση δείκτη περιπτώσεων ελέγχου (Test Cases)
-                int i = 0;
-                bool failed = false;
-
-                // Προσπέλαση και εκτέλεση περιπτώσεων ελέγχου
-                for (i = 0; i < testcases.GetLength(0); i++)
-                // Για κάθε περίπτωση ελέγχου (Test Case), δηλαδή για κάθε γραμμή i του πίνακα testcases
-                {
-                    try
-                    {
-                        // Καλούμε την Assert.AreEqual δίνοντας ως παραμέτρους τα στοιχεία της περίπτωσης ελέγχου,
-                        // δηλαδή τα αντίστοιχα στοιχεία της γραμμής i του πίνακα testcases
-                        string Password = (string)testcases[i, 1];
-                        bool expectedValue = (bool)testcases[i, 2]; // Η τιμή που περιμένω να επιστρέψει η μέθοδος ValidPassword() για την ημερομηνία i
-                        bool actualValue = per.ValidPassword(Password); // Η τιμή που επιστρέφει η μέθοδος ValidPassword() για την ημερομηνία i
-                        Assert.AreEqual(expectedValue, actualValue);
-                    }
-                    catch (Exception e)
-                    {
-                        // Απέτυχε το Test Case
-                        failed = true;
-                        // Καταγράφουμε το Test Case που απέτυχε
-                        Console.WriteLine("Failed Test Case: {0} \n \t Hint: {1} \n \t Reason: {2} ",
-                            (int)testcases[i, 0], (string)testcases[i, 3], e.Message);
-                        //  id, μήνυμα που αιτιολογεί τις πληροφορίες που περιμένω, μήνυμα του exception 
-                    }
-                }
-
-                // Στην περίπτωση που κάποιο Test Case απέτυχε, πέταξε exception.
-                if (failed)
-                    Assert.Fail();
-            
-        }
-
-        [TestMethod]
-        public void TestMethodEncryptPassword() /* OMAR */
-        {
-
-        }
-
-        [TestMethod]
-        public void TestMethodCheckPhone() /* TIGER */
-        {
-            // Δημιουργία ενός αντικειμένου της κλάσης Personnel του HRLib.dll που θέλουμε να τεστάρουμε
-            HRLib.Personnel per = new HRLib.Personnel();
-
-            // Δημιουργία Περιπτώσεων Ελέγχου (Test Cases)
-            object[,] testcases =
-            {
-            /*  { id testcase,  
-             *    τηλέφωνο,
-             *    τύπος τηλεφώνου που περιμένω να επιστρέψει η CheckPhone(),
-             *    πληροφορίες τηλεφώνου που περιμένω να επιστρέψει η CheckPhone(),
-             *    μήνυμα που αιτιολογεί τις πληροφορίες που περιμένω 
-             *  } 
-             */
-                { 1, "2100000000", 0, "Metropolitan Area of Athens - Piraeus", "Σταθερό τηλέφωνο με ζώνη την Μητροπολιτική περιοχή Αθήνας - Πειραιά" },
-                { 2, "2200000000", 0, "Eastern Central Greece, Attica, Aegean Islands", "Σταθερό τηλέφωνο με ζώνη την Ανατολική Στερεά Ελλάδα, Αττική, Νησιά Αιγαίου" },
-                { 3, "2300000000", 0, "Central Macedonia", "Σταθερό τηλέφωνο με ζώνη την Κεντρική Μακεδονία" },
-                { 4, "2400000000", 0, "Thessaly, Western Macedonia", "Σταθερό τηλέφωνο με ζώνη την Θεσσαλία, Δυτική Μακεδονία" },
-                { 5, "2500000000", 0, "Thrace, Eastern Macedonia", "Σταθερό τηλέφωνο με ζώνη την Θράκη, Ανατολική Μακεδονία" },
-                { 6, "2600000000", 0, "Epirus, Western Central Greece, Western Peloponnese, Ionian Islands", "Σταθερό τηλέφωνο με ζώνη την Ήπειρο, Δυτική Στερεά Ελλάδα, Δυτική Πελοπόννησος, Ιόνια Νησιά" },
-                { 7, "2700000000", 0, "Eastern Peloponnese, Kythera", "Σταθερό τηλέφωνο με ζώνη την Ανατολική Πελοπόννησο, Κύθηρα" },
-                { 8, "2800000000", 0, "Crete", "Σταθερό τηλέφωνο με ζώνη την Κρήτη" },
-                { 9, "2900000000", 0, null, "Σταθερό τηλέφωνο με καμία ζώνη" },
-                { 10, "6900000000", 1, "Nova", "Κινητό τηλέφωνο με εταιρία κινητής τηλεφωνίας τη Nova" },
-                { 11, "6930000000", 1, "Nova", "Κινητό τηλέφωνο με εταιρία κινητής τηλεφωνίας τη Nova" },
-                { 12, "6990000000", 1, "Nova", "Κινητό τηλέφωνο με εταιρία κινητής τηλεφωνίας τη Nova" },
-                { 13, "6940000000", 1, "Vodafone", "Κινητό τηλέφωνο με εταιρία κινητής τηλεφωνίας τη Vodafone" },
-                { 14, "6950000000", 1, "Vodafone", "Κινητό τηλέφωνο με εταιρία κινητής τηλεφωνίας τη Vodafone" },
-                { 15, "6970000000", 1, "Cosmote", "Κινητό τηλέφωνο με εταιρία κινητής τηλεφωνίας τη Cosmote" },
-                { 16, "6980000000", 1, "Cosmote", "Κινητό τηλέφωνο με εταιρία κινητής τηλεφωνίας τη Cosmote" },
-                { 17, "6920000000", 1, null, "Κινητό τηλέφωνο με καμία εταιρία κινητής τηλεφωνίας" },
-                { 18, "4562389784", -1, null, "Άκυρο κινητό" },
-                { 19, "210A28221", -1, null, "Άκυρο κινητό" },
-                { 20, "698_141312", -1, null, "Άκυρο κινητό" }
-            };
 
             // Αρχικοποίηση δείκτη περιπτώσεων ελέγχου (Test Cases)
             int i = 0;
@@ -196,20 +127,12 @@ namespace HRLibUnitTest
             {
                 try
                 {
-                    string TestcasePhone = (string)testcases[i, 1];       // Το τηλέφωνο του testcase i 
-                    int ExpectedTypePhone = (int)testcases[i, 2];         // Ο τύπος τηλεφώνου του testcase i που περιμένω να επιστρέψει η CheckPhone() 
-                    string ExpectedInfoPhone = (string)testcases[i, 3];   // Οι πληροφορίες τηλεφώνου του testcase i που περιμένω να επιστρέψει η CheckPhone()
-
-                    // Δήλωση και αρχικοποίηση ref μεταβλητών μεθόδου
-                    int ActualTypePhone = 100;
-                    string ActualInfoPhone = "";
-
-                    per.CheckPhone(TestcasePhone, ref ActualTypePhone, ref ActualInfoPhone);
-
                     // Καλούμε την Assert.AreEqual δίνοντας ως παραμέτρους τα στοιχεία της περίπτωσης ελέγχου,
                     // δηλαδή τα αντίστοιχα στοιχεία της γραμμής i του πίνακα testcases
-                    Assert.AreEqual(ExpectedTypePhone, ActualTypePhone);
-                    Assert.AreEqual(ExpectedInfoPhone, ActualInfoPhone);
+                    string Password = (string)testcases[i, 1];
+                    bool expectedValue = (bool)testcases[i, 2]; // Η τιμή που περιμένω να επιστρέψει η μέθοδος ValidPassword() για την ημερομηνία i
+                    bool actualValue = per.ValidPassword(Password); // Η τιμή που επιστρέφει η μέθοδος ValidPassword() για την ημερομηνία i
+                    Assert.AreEqual(expectedValue, actualValue);
                 }
                 catch (Exception e)
                 {
@@ -225,19 +148,14 @@ namespace HRLibUnitTest
             // Στην περίπτωση που κάποιο Test Case απέτυχε, πέταξε exception.
             if (failed)
                 Assert.Fail();
-        }
-
-        [TestMethod]
-        public void TestMethodInfoEmployee() /* THEO */
-        {
 
         }
-
-        [TestMethod]
-        public void TestMethodLiveInAthens() /* OMAR */
-        {
-
-        }
-
     }
 }
+
+     
+
+        
+
+        
+        
